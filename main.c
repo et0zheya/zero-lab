7440f7f004412a2d6902c031745ed43a86314a6e
Sergei <sergeyskor123@gmail.com>
2021-09-18 01:52:09 +0300

    v1

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a, b, sum;
+    printf("enter your nums \n");
+    scanf("%d%d", &a, &b);
+    sum = a + b;
+    printf("result %d", sum);
+    getchar();
+    getchar();
+    return 0;
+}
